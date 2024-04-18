ce75a297a29433241831737580c3f3211ec9af65
X <154840412+xiamnooxplzdon@users.noreply.github.com>
2024-04-18 09:45:45 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,9 +2,7 @@ import re
 import os
 from os import getenv, environ
 
-api_id = "29917436"
-api_hash = "4a926822b076a086a167fe8f2701d3e9"
-bot_token = "6755014380:AAECxivro_dmP9qPIPmukG5qY6NQm-mUVk4"
+
 
 
 api_id = int(environ.get("API_ID", "29917436"))
