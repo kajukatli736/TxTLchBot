065ed90eac5393fe1da49aa0392ddbdd0102d79b
akpandawep <154840412+xiamnooxplzdon@users.noreply.github.com>
2024-04-18 08:22:58 +0530

    main

--- modules/vars.py
+++
@@ -1,6 +0,0 @@
-# SudoR2spr WOODcraft
-# Add your details here and then deploy by clicking on HEROKU Deploy button
-
-api_id = "29917436"
-api_hash = "4a926822b076a086a167fe8f2701d3e9"
-bot_token = "6755014380:AAECxivro_dmP9qPIPmukG5qY6NQm-mUVk4"
